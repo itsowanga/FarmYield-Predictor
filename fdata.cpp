a5c90be2240e6fec3f87867a5effd55d2dd19deb
itsowanga <sowanga.mbane@gmail.com>
2025-12-11 13:47:39 +0200

    Add initial implementation of farming yield simulator with data input and file output

---
+++ fdata.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream> 
+#include <string>
+
+int main(void){
+
+    std::string crop;
+    std::string rainfall;
+    int pH;
+    int month;
+
+    std::ofstream outputFile("input.txt", std::ios::app);
+
+     if (!outputFile.is_open()) {
+        std::cerr << "Error: Could not open the file for writing." << std::endl;
+        return 1; // Indicate an error
+    }
+
+    while(true){
+        std::cout<<"Farmer input interface:"<<std::endl;
+        // [Crop] [Rainfall] [Soil pH] [Planting month] 
+        std::cout<<"Enter data(space separated) or 'exit' to quit."<<std::endl;
+        std::cin>> crop;
+        if(crop == "exit"){
+            break;
+        }
+        std::cin>> rainfall;
+        std::cin>> pH;
+        while(pH<4 || pH >8){
+            std::cout<<"Value out of bounds. Enter correct pH."<<std::endl;
+            std::cin>> pH;  
+        }
+        std::cin>> month;
+        while(month<1 || month >12){
+            std::cout<<"Value out of bounds. Enter correct month."<<std::endl;
+            std::cin>> month;  
+        }
+
+        std::string input = crop+","+rainfall+","+std::to_string(pH)+","+std::to_string(month);
+        outputFile << input;
+    }
+
+    return 0;
+}
